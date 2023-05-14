4cf5bacf27af1b854d2127791b103f5c6136235b
David Ramirez <dframirez.usmc@gmail.com>
2023-05-13 23:26:18 -0700

    Update setup.sh (needs test)

--- setup.sh
+++ setup.sh
@@ -1,27 +1,33 @@
 # Run this Bash Shell Script after installing Nvidia and Docker requirements.
 #!/bin/bash
+
 # Ubuntu Check
-echo "Check Ubuntu Versions"
-echo "Note: Previously tested on Linux 20.04 LTS"
-echo "Note: Previously tested on Linux Kernel 5.15.0-69-generic"
+echo "\nNOTE: Author has previously tested on:"
+echo "Linux 20.04 LTS"
+echo "Linux Kernel 5.15.0-69-generic"
+echo "\nCHECK UBUNTU VERSIONS"
 uname -r
 uname -m && cat /etc/*release
+
 # Nvidia/CUDA Drivers Check
-echo "Check CUDA Driver Versions"
-echo "Note: Previously tested on NVIDIA Drivers 530.30.02"
-echo "Note: Previously tested on CUDA 11.8.0-520.61.05-1"
-echo "Note: Previously tested on GCC 9.4.0"
+echo "\nNOTE: Author has previously tested on:"
+echo "NVIDIA Drivers 530.30.02"
+echo "CUDA 11.8.0-520.61.05-1"
+echo "GCC 9.4.0"
+echo "\nCHECK NVIDIA VERSIONS"
 cat /proc/driver/nvidia/version
 gcc --version
+
 # Docker Check
-echo "Check Docker Versions"
-echo "Note: Previously tested on Docker 23.0.5, build bc4487a"
-echo "Note: Previously tested on Docker Desktop 4.19.0 (106363)"
-echo "Note: Previously tested on Docker Engine 23.0.5"
-echo "Note: Previously tested on Docker Compose v2.17.3"
+echo "\nNOTE: Author has previously tested on:"
+echo "Docker 23.0.5, build bc4487a"
+echo "Docker Engine 23.0.5"
+echo "Docker Compose v2.17.3"
+echo "\nCHECK DOCKER VERSIONS"
 docker --version
 docker version
 docker compose version
+
 # Download NerfStudio Docker Image
-echo "Downloading NerfStudio Docker Image..."
+echo "\nDownloading NerfStudio Docker Image..."
 docker pull dromni/nerfstudio:0.2.1
